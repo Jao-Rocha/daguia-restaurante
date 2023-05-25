4b2ffa1d9d15aa568a3c556c7919ad2ec1e814f2
João Victor Rocha <joao2013rocha28@hotmail.com>
2023-05-24 22:04:08 -0300

    finalizado

--- Desktop/projeto-restauranteDaguia/restaurante-daguia/src/pages/index.jsx
+++ Desktop/projeto-restauranteDaguia/restaurante-daguia/src/pages/index.jsx
@@ -4,6 +4,7 @@ import Button from '@/components/Button'
 import Map from '@/components/Map'
 import Icons from '@/components/Icons'
 import Navbar from '@/components/Navbar'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -31,28 +32,49 @@ export default function Home() {
         </div>
 
         <section>
-          <div className="text-center mt-10">
-            <p className="text-3xl font-semibold">
-              Restaurante Daguia, a mais de 35 anos trazendo alegria no almoço e
-              no jantar de nossos clientes.
-            </p>
-            <p className="mt-2 m-4 text-justify">
-              O Restaurante Daguia é o mais tradicional comércio da cidade,
-              localizado em Santa Gertrudes – SP. Fundado por Antônio Denardi
-              (Daguia) e Maria Aparecida Denardi (Cidinha), no ano de 1985. A
-              tradição se inicia com uma rotisserie, com o nome “Quero Mais”,
-              onde comercializavam salgados e doces. Com a grande demanda, a
-              necessidade de um espaço maior surge, nascendo assim o atual
-              Restaurante. No novo local, iniciou-se a venda de pratos
-              comerciais durante o almoço, pizzas, lanches e porções no jantar,
-              além de incluir refeições no próprio espaço, devido ao tamanho
-              êxito, a tradição do comércio se estende até os dias atuais, sob o
-              comando da família e dos sócios.
-            </p>
+          <div className="flex flex-col md:flex-row items-center justify-between  p-4 ">
+            <div className="text-center mt-10  md:mb-36 md:w-1/2 md:pr-8">
+              <h2 className="text-3xl text-start font-semibold md:ml-48">
+                Restaurante Daguia, a mais de 35 anos trazendo alegria no almoço
+                e no jantar de nossos clientes.
+              </h2>
+              <p className=" mt-4 mb-0 text-justify md:ml-48 md:mt-6 ">
+                &emsp;O Restaurante Daguia é o mais tradicional comércio da
+                cidade, localizado em Santa Gertrudes – SP. Fundado por Antônio
+                Denardi (Daguia) e Maria Aparecida Denardi (Cidinha), no ano de
+                1985. A tradição se inicia com uma rotisserie, com o nome “Quero
+                Mais”, onde comercializavam salgados e doces. Com a grande
+                demanda, a necessidade de um espaço maior surge, nascendo assim
+                o atual Restaurante.
+              </p>
+              <p className="mt-0 text-justify md:ml-48 ">
+                &emsp;No novo local, iniciou-se a venda de pratos comerciais
+                durante o almoço, pizzas, lanches e porções no jantar, além de
+                incluir refeições no próprio espaço, devido ao tamanho êxito, a
+                tradição do comércio se estende até os dias atuais, sob o
+                comando da família e dos sócios.
+              </p>
+            </div>
+            <div className="md:w-1/2 md:mt-0">
+              <div className="flex justify-center mt-4 md:mt-0">
+                <Image
+                  src="/images/casal.jpeg"
+                  width={500}
+                  height={200}
+                  alt="Casal fundador do restaurante."
+                />
+              </div>
+
+              <i className="mt-2 flex justify-center">
+                Antônio Denardi (Daguia) e Maria Aparecida Denardi (Cidinha).
+              </i>
+            </div>
           </div>
         </section>
-
-        <section className="m-10">
+        <section className="m-10 mt-2 md:mt-0">
+          <h2 className="text-3xl font-semibold text-center">
+            Alguns dos nossos pratos.
+          </h2>
           <div>
             <CardFood
               image={'/images/prato1.jfif'}
@@ -78,30 +100,22 @@ export default function Home() {
           </div>
         </section>
         <section className="m-10">
-          <div className="text-center mt-10">
-            <h2 className="text-3xl font-semibold">
-              O que você encontrará em nosso restaurante ?
+          <div className="mt-10">
+            <h2 className="text-3xl text-center font-semibold ">
+              O que você encontrará em nosso restaurante?
             </h2>
-            <ul>
-              <li className="text-lg text-center ">
-                Temos nosso incrível self-service , com diversas opções para
-                serem provados. No qual voçê pode optar por comer a vontade ou
-                comer por quilo!
-              </li>
-              <li className="text-lg text-center">
-                Possuimos uma grande variadade de pratos incriveis para você e
-                sua familia, que podem ser pedidos tanto no almoço quanto no
-                jantar.
-              </li>
-              <li className="text-lg text-center">
-                Temos também uma grande variedade de porções e pizzas , além de
-                sucos e nossa famosa caipirinha.
-              </li>
-              <li className="text-lg text-center">
-                E o melhor , tudo isso pode ser entregue no conforto da sua
-                casa.
-              </li>
-            </ul>
+            <p className="text-lg  text-justify mt-4 md:text-center md:ml-80 md:mr-80">
+              &emsp;Temos nosso incrível self-service , com diversas opções para
+              serem provados. No qual você pode optar por comer a vontade ou
+              comer por quilo!
+            </p>
+            <p className="text-lg text-justify mt-0 md:text-center md:ml-80 md:mr-80">
+              &emsp;Possuímos uma grande variedade de pratos incríveis para você
+              e sua família, que podem ser pedidos tanto no almoço quanto no
+              jantar. Temos também uma grande variedade de porções e pizzas,
+              além de sucos e nossa famosa caipirinha. E o melhor, tudo isso
+              pode ser entregue no conforto da sua casa.
+            </p>
           </div>
         </section>
         <section className="m-10 p-4 w-full ml-0">
